3f33140e2d7ffe6c4e90b5896959c774a2125bd7
StevenMElliott <smonroeelliott@gmail.com>
2020-07-17 21:29:32 -0600

    all good except stretch and oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,44 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    def __init__(self, vehicle):
+        self.vehicle = vehicle
+
+
+#child of vehicle
+class FlightVehicle(Vehicle):
+    def __init__(self, vehicle, flightvehicle):
+        super().__init__(vehicle)
+        self.flightvehicle = flightvehicle
+
+#child of flight vehicle
+class Starship(FlightVehicle):
+    def __init__(self, vehicle, flightvehicle, starship):
+        super().__init__(vehicle, flightvehicle)
+        self.starship = starship
+
+#child of flight vehicle
+class Airplane(FlightVehicle):
+    def __init__(self, vehicle, flightvehicle, airplane):
+        super().__init__(vehicle, flightvehicle)
+        self.airplane = airplane
+
+#child of vehicle
+class GroundVehicle(Vehicle):
+    def __init__(self, vehicle, groundvehicle):
+        super().__init__(vehicle)
+        self.groundvehicle = groundvehicle
+
+#child of ground vehicle
+class Car(GroundVehicle):
+    def __init__(self, vehicle, groundvehicle, car):
+        super().__init__(vehicle, groundvehicle)
+        self.car = car
+
+#child of ground vehicle
+class Motorcycle(GroundVehicle):
+    def __init__(self, vehicle, groundvehicle, motorcycle):
+        super().__init__(vehicle, groundvehicle)
+        self.motorcycle = motorcycle
